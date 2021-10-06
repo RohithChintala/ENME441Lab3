a00ab315ba0e90dd1b1b97362a30d47dbf7a634a
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-06 20:38:32 +0000

    test

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import PCF8591
 
 class Joystick:
-  def __init__(self, bus, address):
+  def __init__(self, bus, address, p, q):
     self.adc = PCF8591(bus, address)
     def getX():
+      self.p =  adc.address
       return(self.PCF8591.read(0))
     def getY():
+      self.q = adc.addre
       return(self.PCF8591.read(1))
 
 
