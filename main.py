492536cf829a863e4adb917bdcfbc1fe5bf0d3bc
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-06 20:49:47 +0000

    s

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from PCF8591 import PCF8591
 class Joystick:
   def __init__(self, address):
     self.adc = PCF8591(address)
-    def getX():
-      return(self.adc.read(0))
-    def getY():
-      return(self.adc.read(1))
+  def getX():
+    return(self.adc.read(0))
+  def getY():
+    return(self.adc.read(1))
 
 
 try:
