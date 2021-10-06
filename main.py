eb413733d5daf9b1f35ef92e231266a5326a0074
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-06 20:27:54 +0000

    test

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ class Joystick:
   def __init__(self, bus, address):
     self.adc = PCF8591(bus, address)
     def getX():
-      return(read(0))
+      return(PCF8591.read(0))
     def getY():
-      return(read(1))
+      return(PCF8591.read(1))
 
 
 try:
