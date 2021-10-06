da36ca7f9b1d4c9028a6bce32c6d6cfeb0ce90de
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-06 20:51:18 +0000

    g

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ class Joystick:
 try:
   while 1:
     J = Joystick(0x48)
-    print(J.getX())
+    print(J.getX)
     sleep(0.5)
-    print(J.getY())
+    print(J.getY)
     sleep(0.5)
 except KeyboardInterrupt:
  print('\nExiting')
