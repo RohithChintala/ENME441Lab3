05ac93d9385d257375cdceb98cb46349372d2a47
Rohith Chintala <35852989+RohithChintala@users.noreply.github.com>
2021-10-13 19:02:21 +0000

    commented

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-from PCF8591 import PCF8591
-from time import sleep
-import smbus
+from PCF8591 import PCF8591 #imports PCF8591 class
+from time import sleep #imports sleep
+#import smbus
 
-class Joystick:
-  def __init__(self, address):
-    self.adc = PCF8591(address)
-  def getX(self):
-    return(self.adc.read(0))
-  def getY(self):
-    return(self.adc.read(1))
+class Joystick: #creates joystick class
+  def __init__(self, address): #instantiates address 
+    self.adc = PCF8591(address) #calls PCF8591 class by composition
+  def getX(self): #creates class method
+    return(self.adc.read(0)) #returns adc read of address 0
+  def getY(self): #creates calss method
+    return(self.adc.read(1)) #returns adc read of address 0
 
 
-try:
-  while 1:
-    J = Joystick(0x48)
-    #print(J.getX(), J.getY())
-    #print('%d, %d' % (J.getX(),J.getY()))
-    print("{:>3}, {:>3}".format(J.getX(), J.getY()))
-    sleep(0.1)
-except KeyboardInterrupt:
+try: #runs if no error
+  while 1: #runs infinite loop
+    J = Joystick(0x48) #creates J object from joystick class
+    print("{:>3}, {:>3}".format(J.getX(), J.getY())) 
+    #prints X and Y values in proper format
+    sleep(0.1) # sleeps for 100 ms
+except KeyboardInterrupt: #ends code with keyboard interupt
  print('\nExiting')
